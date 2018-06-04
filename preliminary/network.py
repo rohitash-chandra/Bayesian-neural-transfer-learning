560df5152b2de7b3a1690a1814bc34ebeaa74225
arpit-kapoor <kapoor.arpit97@gmail.com>
2018-06-04 13:08:00 +1000

    network file created

---
+++ preliminary/network.py
@@ -0,0 +1,143 @@
+# i/usr/bin/python
+
+import matplotlib.pyplot as plt
+import numpy as np
+import random
+import time
+from scipy.stats import multivariate_normal
+from scipy.stats import norm
+import math
+import os
+from expdata import setexperimentdata
+import sys
+
+# An example of a class
+class Network:
+    def __init__(self, Topo, Train, Test, learn_rate):
+        self.Top = Topo  # NN topology [input, hidden, output]
+        self.TrainData = Train
+        self.TestData = Test
+        np.random.seed()
+        self.lrate = learn_rate
+
+        self.W1 = np.random.randn(self.Top[0], self.Top[1]) / np.sqrt(self.Top[0])
+        self.B1 = np.random.randn(1, self.Top[1]) / np.sqrt(self.Top[1])  # bias first layer
+        self.W2 = np.random.randn(self.Top[1], self.Top[2]) / np.sqrt(self.Top[1])
+        self.B2 = np.random.randn(1, self.Top[2]) / np.sqrt(self.Top[1])  # bias second layer
+
+        self.hidout = np.zeros((1, self.Top[1]))  # output of first hidden layer
+        self.out = np.zeros((1, self.Top[2]))  # output last layer
+
+    def sigmoid(self, x):
+        return 1 / (1 + np.exp(-x))
+
+    def sampleEr(self, actualout):
+        error = np.subtract(self.out, actualout)
+        sqerror = np.sum(np.square(error)) / self.Top[2]
+        return sqerror
+
+    def ForwardPass(self, X):
+        z1 = X.dot(self.W1) - self.B1
+        self.hidout = self.sigmoid(z1)  # output of first hidden layer
+        z2 = self.hidout.dot(self.W2) - self.B2
+        self.out = self.sigmoid(z2)  # output second hidden layer
+
+    def BackwardPass(self, Input, desired):
+        out_delta = (desired - self.out) * (self.out * (1 - self.out))
+        hid_delta = out_delta.dot(self.W2.T) * (self.hidout * (1 - self.hidout))
+
+        #self.W2 += (self.hidout.T.dot(out_delta) * self.lrate)
+        #self.B2 += (-1 * self.lrate * out_delta)
+        #self.W1 += (Input.T.dot(hid_delta) * self.lrate)
+        #self.B1 += (-1 * self.lrate * hid_delta)
+
+        layer = 1  # hidden to output
+        for x in xrange(0, self.Top[layer]):
+            for y in xrange(0, self.Top[layer + 1]):
+                self.W2[x, y] += self.lrate * out_delta[y] * self.hidout[x]
+        for y in xrange(0, self.Top[layer + 1]):
+            self.B2[y] += -1 * self.lrate * out_delta[y]
+
+        layer = 0  # Input to Hidden
+        for x in xrange(0, self.Top[layer]):
+            for y in xrange(0, self.Top[layer + 1]):
+                self.W1[x, y] += self.lrate * hid_delta[y] * Input[x]
+        for y in xrange(0, self.Top[layer + 1]):
+            self.B1[y] += -1 * self.lrate * hid_delta[y]
+
+    def decode(self, w):
+        w_layer1size = self.Top[0] * self.Top[1]
+        w_layer2size = self.Top[1] * self.Top[2]
+
+        w_layer1 = w[0:w_layer1size]
+        self.W1 = np.reshape(w_layer1, (self.Top[0], self.Top[1]))
+
+        w_layer2 = w[w_layer1size:w_layer1size + w_layer2size]
+        self.W2 = np.reshape(w_layer2, (self.Top[1], self.Top[2]))
+        self.B1 = w[w_layer1size + w_layer2size:w_layer1size + w_layer2size + self.Top[1]]
+        self.B2 = w[w_layer1size + w_layer2size + self.Top[1]:w_layer1size + w_layer2size + self.Top[1] + self.Top[2]]
+
+
+    def encode(self):
+        w1 = self.W1.ravel()
+        w2 = self.W2.ravel()
+        w = np.concatenate([w1, w2, self.B1, self.B2])
+        return w
+
+    def langevin_gradient(self, data, w, depth):  # BP with SGD (Stocastic BP)
+
+        self.decode(w)  # method to decode w into W1, W2, B1, B2.
+        size = data.shape[0]
+
+        Input = np.zeros((1, self.Top[0]))  # temp hold input
+        Desired = np.zeros((1, self.Top[2]))
+        fx = np.zeros(size)
+
+        for i in xrange(0, depth):
+            for i in xrange(0, size):
+                pat = i
+                Input = data[pat, 0:self.Top[0]]
+                Desired = data[pat, self.Top[0]:]
+                self.ForwardPass(Input)
+                self.BackwardPass(Input, Desired)
+
+        w_updated = self.encode()
+
+        return  w_updated
+
+    def evaluate_proposal(self, data, w ):  # BP with SGD (Stocastic BP)
+
+        self.decode(w)  # method to decode w into W1, W2, B1, B2.
+        size = data.shape[0]
+
+        Input = np.zeros((1, self.Top[0]))  # temp hold input
+        Desired = np.zeros((1, self.Top[2]))
+        fx = np.zeros((size,self.Top[2]))
+
+        for i in xrange(0, size):  # to see what fx is produced by your current weight update
+            Input = data[i, 0:self.Top[0]]
+            self.ForwardPass(Input)
+            fx[i] = self.out
+
+        return fx
+
+
+
+# --------------------------------------------------------------------------
+
+def covert_time(secs):
+    if secs >= 60:
+        mins = str(secs/60)
+        secs = str(secs%60)
+    else:
+        secs = str(secs)
+        mins = str(00)
+
+    if len(mins) == 1:
+        mins = '0'+mins
+
+    if len(secs) == 1:
+        secs = '0'+secs
+
+    return [mins, secs]
+
